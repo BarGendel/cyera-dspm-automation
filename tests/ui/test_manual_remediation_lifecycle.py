af4a0081e8318fdcde47bc753beef75bca62ed2e
Victor Gendel <victorgendel@Victors-MacBook-Pro.local>
2026-05-28 11:53:41 +0300

    Update manual remediation UI tests

--- tests/ui/test_manual_remediation_lifecycle.py
+++ tests/ui/test_manual_remediation_lifecycle.py
@@ -20,14 +20,6 @@ class TestManualRemediationLifecycle:
         self.alerts_list = AlertsListPage(page)
         self.alert_detail_drawer = AlertDetailDrawer(page)
 
-    def test_open_alert_cannot_jump_to_resolved(self, api_client) -> None:
-        alert = api_client.find_alert(statuses=[AlertStatus.OPEN], auto_remediate=False)
-        self.alerts_list.open_alert(alert)
-
-        self.alert_detail_drawer.open_status_dropdown()
-        expect(self.alert_detail_drawer.status_option(AlertStatus.IN_PROGRESS)).to_be_visible()
-        expect(self.alert_detail_drawer.status_option(AlertStatus.RESOLVED)).not_to_be_visible()
-
     @pytest.mark.e2e
     def test_manual_remediation_alert_lifecycle(self, api_client) -> None:
         alert = api_client.find_alert(statuses=[AlertStatus.OPEN], auto_remediate=False)
@@ -48,24 +40,3 @@ class TestManualRemediationLifecycle:
 
         expect(self.alert_detail_drawer.status_button).to_contain_text(AlertStatus.RESOLVED)
         expect(self.alert_detail_drawer.page.get_by_text(FINAL_COMMENT)).to_be_visible()
-
-    def test_resolved_alert_can_be_reopened_and_moved_to_in_progress(self, api_client) -> None:
-        alert = api_client.find_alert(statuses=[AlertStatus.OPEN], auto_remediate=False)
-        self.alerts_list.open_alert(alert)
-
-        self.alert_detail_drawer.set_status(AlertStatus.IN_PROGRESS)
-        self.alert_detail_drawer.assign(ASSIGNEE)
-        self.alert_detail_drawer.add_remediation_notes(REMEDIATION_NOTES)
-        self.alert_detail_drawer.remediate()
-        self.alert_detail_drawer.set_status(AlertStatus.RESOLVED)
-        self.alert_detail_drawer.add_comment(FINAL_COMMENT)
-        expect(self.alert_detail_drawer.status_button).to_contain_text(AlertStatus.RESOLVED)
-
-        self.alert_detail_drawer.open_status_dropdown()
-        self.alert_detail_drawer.status_option("Reopened").click()
-        expect(self.alert_detail_drawer.status_button).to_contain_text("Reopened")
-
-        self.alert_detail_drawer.open_status_dropdown()
-        expect(self.alert_detail_drawer.status_option(AlertStatus.IN_PROGRESS)).to_be_visible()
-        self.alert_detail_drawer.status_option(AlertStatus.IN_PROGRESS).click()
-        expect(self.alert_detail_drawer.status_button).to_contain_text(AlertStatus.IN_PROGRESS)
